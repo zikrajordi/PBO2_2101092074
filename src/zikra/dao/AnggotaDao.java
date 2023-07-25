d71d1caed1a4d63d8d7418b81b15a56b6ddcd50d
zikrajordi <114377127+zikrajordi@users.noreply.github.com>
2023-07-25 14:10:07 +0700

    Create AnggotaDao.java

---
+++ src/zikra/dao/AnggotaDao.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zikra.dao;
+
+import java.sql.Connection;
+import java.util.List;
+import zikra.model.Anggota;
+
+
+
+
+/**
+ *
+ * @author User
+ */
+public interface AnggotaDao {
+    public void insert(Connection con, Anggota anggota) throws Exception;
+    public void update(Connection con, Anggota anggota) throws Exception;
+    public void delete(Connection con, Anggota anggota) throws Exception;
+    public Anggota getAnggota(Connection con, String kode) throws Exception;
+    public List<Anggota> getAllAnggota(Connection con) throws Exception;
+}
