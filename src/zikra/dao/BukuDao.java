f0da269d65d937a508e5889ca52937537d4c0a3c
zikrajordi <114377127+zikrajordi@users.noreply.github.com>
2023-07-25 14:10:18 +0700

    Create BukuDao.java

---
+++ src/zikra/dao/BukuDao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zikra.dao;
+
+import java.sql.Connection;
+import java.util.List;
+import zikra.model.Buku;
+
+/**
+ *
+ * @author User
+ */
+public interface BukuDao {
+    public void insert(Connection con, Buku buku) throws Exception;
+    public void update(Connection con, Buku buku) throws Exception;
+    public void delete(Connection con, Buku buku) throws Exception;
+    public Buku getBuku(Connection con, String kode) throws Exception;
+    public List<Buku> getAllBuku(Connection con) throws Exception;
+}
