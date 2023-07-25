7893202abb3c4b46c72bd0a9ec6cf18adf50f823
zikrajordi <114377127+zikrajordi@users.noreply.github.com>
2023-07-25 14:10:52 +0700

    Create Pengembalian.java

---
+++ src/zikra/model/Pengembalian.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zikra.model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author User
+ */
+public class Pengembalian {
+    private String kodeanggota;
+    private String kodebuku;
+    private String tglpinjam;
+    private String tgldikembalikan;
+    private int terlambat;
+    private double denda;
+
+    public Pengembalian() {
+    }
+
+    public String getKodeanggota() {
+        return kodeanggota;
+    }
+
+    public void setKodeanggota(String kodeanggota) {
+        this.kodeanggota = kodeanggota;
+    }
+
+    public String getKodebuku() {
+        return kodebuku;
+    }
+
+    public void setKodebuku(String kodebuku) {
+        this.kodebuku = kodebuku;
+    }
+
+    public String getTglpinjam() {
+        return tglpinjam;
+    }
+
+    public void setTglpinjam(String tglpinjam) {
+        this.tglpinjam = tglpinjam;
+    }
+
+    public String getTgldikembalikan() {
+        SimpleDateFormat s = new SimpleDateFormat("yyyy-MM-dd");
+        Date tgl = new Date();
+        tgldikembalikan = s.format(tgl);
+        return tgldikembalikan;
+    }
+
+    public void setTgldikembalikan(String tgldikembalikan) {
+        this.tgldikembalikan = tgldikembalikan;
+    }
+
+    public int getTerlambat() {
+        return terlambat;
+    }
+
+    public void setTerlambat(int terlambat) {
+        this.terlambat = terlambat;
+    }
+
+    public double getDenda() {
+        denda = terlambat * 1000;
+        return denda;
+    }
+
+    public void setDenda(double denda) {
+        this.denda = denda;
+    }
+    
+    
+}
